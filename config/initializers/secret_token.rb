066abe42fd04c66ac6045c30b77e90248e014e72
Diego Marcilio <dvmarcilio@gmail.com>
2013-08-31 10:57:58 -0300

    Rails 3.2.14

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MeAjudaNisso::Application.config.secret_token = 'cafe0ab608ffab36e3024b63efee4a83a15313bc98dd96aeecd3378607bcba21162767d4ad093d485316ceb94c48f6d81757c39969eb3f7c22ea1c1018f3939b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MeAjudaNisso::Application.config.session_store :cookie_store, key: '_me_ajuda_nisso_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MeAjudaNisso::Application.config.session_store :active_record_store
