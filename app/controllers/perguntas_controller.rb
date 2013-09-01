d3916b0e227b6a0b6d4199450f66bdf03dcdef0b
Diego Marcilio <dvmarcilio@gmail.com>
2013-09-01 17:00:43 -0300

    Passando a geração de perguntas para o Controller

--- app/controllers/perguntas_controller.rb
+++ app/controllers/perguntas_controller.rb
@@ -1,5 +1,16 @@
+# encoding: utf-8
+
+Pergunta = Struct.new :titulo, :votos
+
 class PerguntasController < ApplicationController
 	def bem_votadas
-	
+		@perguntas = Object.new
+		def @perguntas.each(&block)
+			[
+				Pergunta.new("Como testar conteúdo de tabela no Cucumber", 2),
+				Pergunta.new("O que é um gemset no RVM ?", 10),
+				Pergunta.new("invalid multibyte char (US-ASCII) with Rails and Ruby 1.9", 40)
+			].each(&block)
+		end
 	end
 end
