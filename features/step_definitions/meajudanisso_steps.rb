d5cdf12cffe254dfdc94b1c0d359ff15c58d85b7
Diego Marcilio <dvmarcilio@gmail.com>
2013-09-01 15:42:46 -0300

    Passando o 2o cenario 'Visualizar perguntas bem votadas' com valores HTML hard-coded

--- features/step_definitions/meajudanisso_steps.rb
+++ features/step_definitions/meajudanisso_steps.rb
@@ -11,11 +11,26 @@ Então(/^eu devo ver que não existem perguntas com votos$/) do
   page.should have_content("Não existem perguntas com votos")
 end
 
+Pergunta = Struct.new :titulo, :votos
 Dado(/^que existem perguntas com votos$/) do
+	@perguntas = [
+		Pergunta.new("Como testar conteúdo de tabela no Cucumber", 2),
+		Pergunta.new("O que é um gemset no RVM ?", 10),
+		Pergunta.new("invalid multibyte char (US-ASCII) with Rails and Ruby 1.9", 40)
+	]
+end
+
+Então(/^eu devo ver as perguntas com sua quantidade de votos$/) do
+  @perguntas.each do |pergunta|
+  	within_table('perguntas') do
+  		page.should have_css("td", text: pergunta.titulo)
+  		page.should have_css("td", text: pergunta.votos)
+  	end
+  end
+end
+
+Então(/^eu devo ver as perguntas ordenadas decrescentemente pela quantidade de votos$/) do
   pending # express the regexp above with the code you wish you had
 end
 
-Então(/^eu devo ver perguntas ordenadas decrescentemente pela quantidade de votos$/) do
-  pending # express the regexp above with the code you wish you had
-end
 
