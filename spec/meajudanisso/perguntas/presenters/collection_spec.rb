1cd889b703d74ac04488ff1d53b892de24faeab1
Diego Marcilio <dvmarcilio@gmail.com>
2013-09-03 19:47:56 -0300

    Limpando o teste 'expoe oe as n perguntas com mais votos' usando let .

--- spec/meajudanisso/perguntas/presenters/collection_spec.rb
+++ spec/meajudanisso/perguntas/presenters/collection_spec.rb
@@ -5,16 +5,17 @@ require 'perguntas/presenters/collection'
 describe MeAjudaNisso::Perguntas::Presenters::Collection do
 	context "pegando as perguntas mais bem votadas" do
 		qtde = 3
+		let(:p1) {double("p1", votos: 10) }
+		let(:p2) {double("p2", votos: 20) }
+		let(:p3) {double("p3", votos: 30) }
+		let(:p4) {double("p4", votos: 40) }
+		let(:p5) {double("p5", votos: 50) }
+		let(:perguntas) { [p1, p2, p3, p4, p5] }
+		let(:presenter) { MeAjudaNisso::Perguntas::Presenters::Collection.for(perguntas) }
+		
 		it "expõe as #{qtde} perguntas com mais votos" do
-			p1 = double("p1", votos: 10)
-			p2 = double("p2", votos: 20)
-			p3 = double("p3", votos: 30)
-			p4 = double("p4", votos: 40)
-			p5 = double("p5", votos: 50)
-			perguntas = [p1, p2, p3, p4, p5]
-			
-			presenter = MeAjudaNisso::Perguntas::Presenters::Collection.for(perguntas)
 			expect{|b| presenter.bem_votadas(&b) }.to yield_successive_args(p5,p4,p3)
 		end
+		
 	end
 end
