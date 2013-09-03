429c13424a345948ec45a70738773a80cc8bec00
Diego Marcilio <dvmarcilio@gmail.com>
2013-09-03 19:38:54 -0300

    Presenter de perguntas consegue apresentar as 3(hard-coded) perguntas mais votadas.

---
+++ app/meajudanisso/perguntas/presenters/collection.rb
@@ -0,0 +1,25 @@
+module MeAjudaNisso
+	module Perguntas
+		module Presenters
+			class Collection
+
+				@@qtde = 3
+
+				def initialize(perguntas)
+					@perguntas = perguntas
+				end
+			
+				attr_reader :perguntas
+				def self.for(perguntas)
+					new(perguntas)
+				end
+				
+				def bem_votadas(&block)
+					self.perguntas.sort_by{|p| p.votos}.reverse.take(@@qtde).each(&block)
+				end
+				
+			end
+			
+		end
+	end
+end
