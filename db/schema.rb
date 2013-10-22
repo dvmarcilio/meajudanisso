034a6cbe3d73ed5317450846442f9d0623e1db81
Diego Marcilio <dvmarcilio@gmail.com>
2013-10-22 15:44:22 -0200

    Removendo tabela comments. Vai ser adicionada depois

--- db/schema.rb
+++ db/schema.rb
@@ -11,26 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131022172102) do
+ActiveRecord::Schema.define(:version => 20131022173734) do
 
   create_table "answers", :force => true do |t|
     t.text    "conteudo"
     t.integer "question_id"
   end
-
-  create_table "comments", :force => true do |t|
-    t.string   "title",            :limit => 50, :default => ""
-    t.text     "comment"
-    t.integer  "commentable_id"
-    t.string   "commentable_type"
-    t.integer  "user_id"
-    t.datetime "created_at",                                     :null => false
-    t.datetime "updated_at",                                     :null => false
-  end
-
-  add_index "comments", ["commentable_id"], :name => "index_comments_on_commentable_id"
-  add_index "comments", ["commentable_type"], :name => "index_comments_on_commentable_type"
-  add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
 
   create_table "punches", :force => true do |t|
     t.integer  "punchable_id",                                :null => false
