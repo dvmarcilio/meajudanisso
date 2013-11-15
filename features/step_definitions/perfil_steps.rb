5b975805f44080752207d304455e8677e1bcf021
Diego Marcilio <dvmarcilio@gmail.com>
2013-11-15 17:28:40 -0200

    Cenário 'Mostra a quantidade de perguntas feita pelo usuário' escrito e já passando

--- features/step_definitions/perfil_steps.rb
+++ features/step_definitions/perfil_steps.rb
@@ -12,11 +12,27 @@ Quando(/^eu visito a página do seu perfil$/) do
 end
 
 Então(/^eu devo ver uma mensagem de que ele não fez nenhuma pergunta$/) do
-  within(div_perguntas) { page.should have_text 'O usuário ainda não fez uma pergunta.' }
+  within(div_perguntas) { page.should have_css qtde_perguntas, 'O usuário ainda não fez uma pergunta.' }
 end
+
+Dado(/^que ele fez (\d+) perguntas$/) do |count|
+  Integer(count).times { FactoryGirl.create(:question, user: @user) } 
+end
+
+Então(/^eu devo ver que ele fez (\d+) perguntas$/) do |count|
+  helper = Aview.new
+  within(div_perguntas) { page.should have_css qtde_perguntas, text: (helper.pluralize(count, 'Pergunta', 'Perguntas')) }
+end
+
 
 private
   def div_perguntas
     '#perguntas'
   end
+  
+  def qtde_perguntas
+    '#qtde_perguntas'
+  end
+  
+  
 
--- features/visualizar_perfil_usuario.feature
+++ features/visualizar_perfil_usuario.feature
@@ -6,3 +6,15 @@ Funcionalidade: Visualizar perfil do usuário
     E que ele não fez nenhuma pergunta
     Quando eu visito a página do seu perfil
     Então eu devo ver uma mensagem de que ele não fez nenhuma pergunta
+    
+  Esquema do Cenário: Mostra a quantidade de perguntas feita pelo usuário
+    Dado que um usuário cadastrado existe
+    E que ele fez <quantidade> perguntas
+    Quando eu visito a página do seu perfil
+    Então eu devo ver que ele fez <quantidade> perguntas
+    
+    Exemplos:
+      | quantidade |
+      | 1          |
+      | 5          |
+      | 10         |
