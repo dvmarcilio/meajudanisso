d4feafa25884892f40178ac42e22c06e79855a28
Diego Marcilio <dvmarcilio@gmail.com>
2013-11-20 16:52:36 -0200

    Tentando heroku

--- config/application.rb
+++ config/application.rb
@@ -50,6 +50,9 @@ module MeAjudaNisso
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
+    
+    # HEROKU
+    config.assets.initialize_on_precompile = false
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
