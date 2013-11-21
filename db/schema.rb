3994c6eb64e397ca476b648412b9a5927db19d30
Diego Marcilio <dvmarcilio@gmail.com>
2013-11-21 01:31:19 -0200

    Gema e formatações do Relish

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131109012134) do
+ActiveRecord::Schema.define(:version => 20131120204159) do
 
   create_table "answers", :force => true do |t|
     t.text     "conteudo"
---
+++ features/README.md
@@ -0,0 +1,4 @@
+Projeto acadêmico por Diego Marcílio e Bruno Andrade.   
+[Github][1]
+
+[1]: http://github.com/dvmarcilio/meajudanisso
